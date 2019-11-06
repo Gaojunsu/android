f0d3ec812504db592b44812d6ba406f219f227b2
jean <jean_su@hanbo.com>
2019-11-06 14:38:34 +0800

    recycleView

--- app/src/main/java/com/keylinks/android_day/MainActivity.java
+++ app/src/main/java/com/keylinks/android_day/MainActivity.java
@@ -1,13 +1,96 @@
 package com.keylinks.android_day;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView mRecyclerView;
+    private FeedAdapter mFeedAdapter;
+    private RelativeLayout mSuspensionBar;
+    private TextView mSuspensionTv;
+    private ImageView mSuspensionIv;
+    private int mSuspensionBarHeight;
+    private int mCurrentPosition;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSuspensionBar = findViewById(R.id.suspension_bar);
+        mSuspensionTv = findViewById(R.id.tv_nickname);
+        mSuspensionIv = findViewById(R.id.iv_avatar);
+        mRecyclerView = findViewById(R.id.recyclerView);
+
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mFeedAdapter=new FeedAdapter();
+        mRecyclerView.setAdapter(mFeedAdapter);
+        mRecyclerView.setHasFixedSize(true);
+        //滚动监听
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                //获取悬浮条的高度
+                mSuspensionBarHeight = mSuspensionBar.getHeight();
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                //对悬浮条的位置进行调整
+                //找到下一个itemView
+                View view  = layoutManager.findViewByPosition(mCurrentPosition + 1);
+                if (view!=null){
+                    if (view.getTop()<=mSuspensionBarHeight){
+                        //移动
+                        mSuspensionBar.setY(-(mSuspensionBarHeight-view.getTop()));
+                    }else{
+                        //保持原来的位置
+                        mSuspensionBar.setY(0);
+                    }
+                }
+                if (mCurrentPosition!=layoutManager.findFirstVisibleItemPosition()){
+                    mCurrentPosition=layoutManager.findFirstVisibleItemPosition();
+                    updateSuspensionBar();
+                }
+            }
+        });
+        updateSuspensionBar();
+
+    }
+
+    private void updateSuspensionBar() {
+
+        Picasso.with(this)
+                .load(getAvatarResId(mCurrentPosition))
+                .centerInside()
+                .fit()
+                .into(mSuspensionIv);
+        mSuspensionTv.setText("NetEase "  + mCurrentPosition);
+    }
+
+    private int getAvatarResId(int mCurrentPosition) {
+
+        switch (mCurrentPosition % 4){
+            case 0:
+                return R.drawable.avatar1;
+            case 1:
+                return R.drawable.avatar2;
+            case 2:
+                return R.drawable.avatar3;
+            case 3:
+                return R.drawable.avatar4;
+        }
+        return 0;
     }
 }
